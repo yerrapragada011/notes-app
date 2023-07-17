c55ca5964e5cf7acfd68d0ea32e212a6438a458a
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-17 14:58:12 -0400

    Listing Notes

--- notes.js
+++ notes.js
@@ -38,6 +38,16 @@ const removeNote = (title) => {
   }
 };
 
+const listNotes = () => {
+  const notes = loadNotes();
+
+  console.log(chalk.inverse('Your notes'));
+
+  notes.forEach((note) => {
+    console.log(note.title);
+  });
+};
+
 const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJSON);
@@ -57,4 +67,5 @@ module.exports = {
   getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
+  listNotes: listNotes,
 };
