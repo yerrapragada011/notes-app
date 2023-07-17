656780b82174261e873b4c89cb5febd93ed003e4
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-17 14:50:05 -0400

    Refractoring to Use Arrow Functions

--- notes.js
+++ notes.js
@@ -1,13 +1,13 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-const getNotes = function () {
+const getNotes = () => {
   return 'Your notes...';
 };
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter(function (note) {
+  const duplicateNotes = notes.filter((note) => {
     return note.title === title;
   });
 
@@ -24,9 +24,9 @@ const addNote = function (title, body) {
   }
 };
 
-const removeNote = function (title) {
+const removeNote = (title) => {
   const notes = loadNotes();
-  const notesToKeep = notes.filter(function (note) {
+  const notesToKeep = notes.filter((note) => {
     return note.title !== title;
   });
 
@@ -38,12 +38,12 @@ const removeNote = function (title) {
   }
 };
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJSON);
 };
 
-const loadNotes = function () {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync('notes.json');
     const dataJSON = dataBuffer.toString();
