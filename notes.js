299fe2f8a6179e56dda26f8f5031d398e0b376be
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-07-17 13:52:51 -0400

    Removing a Note

--- notes.js
+++ notes.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const fs = require('fs');
 
 const getNotes = function () {
@@ -17,9 +18,23 @@ const addNote = function (title, body) {
     });
 
     saveNotes(notes);
-    console.log('New note added!');
+    console.log(chalk.green.inverse('New note added!'));
   } else {
-    console.log('Note title taken!');
+    console.log(chalk.red.inverse('Note title taken!'));
+  }
+};
+
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(function (note) {
+    return note.title !== title;
+  });
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed!'));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse('No note found!'));
   }
 };
 
@@ -41,4 +56,5 @@ const loadNotes = function () {
 module.exports = {
   getNotes: getNotes,
   addNote: addNote,
+  removeNote: removeNote,
 };
